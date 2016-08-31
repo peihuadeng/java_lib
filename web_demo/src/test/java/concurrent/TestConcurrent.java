5789a779e37a081f1f11757e9158be1cafc077fb
peihuadeng <peihuadeng@foxmail.com>
2016-08-31 11:07:42 +0800

    1.maven对webservice客户端打包jar 2.log4j日志修改 3.并发测试用例

---
+++ web_demo/src/test/java/concurrent/TestConcurrent.java
@@ -0,0 +1,38 @@
+package concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.Test;
+
+public class TestConcurrent {
+	
+	private class MyRunnable implements Runnable {
+
+		@Override
+		public void run() {
+			int rand = (int) (Math.random() * 10000);
+			System.out.println(rand + ": start");
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(rand + ": end");
+		}
+	}
+	
+	@Test
+	public void mainTest() throws InterruptedException {
+		ExecutorService service = Executors.newCachedThreadPool();
+		MyRunnable runnable = new MyRunnable();
+		for (int i = 0; i < 5; i ++) {
+			service.execute(runnable);
+		}
+
+		Thread.sleep(10000);
+		
+	}
+
+}
